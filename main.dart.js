ae90dd2c999aa4baa8784c01501f2e07e13bc062
f2janyway <f2janyway@gmail.com>
2023-05-10 22:15:40 +0900

    Add assets

--- main.dart.js
+++ main.dart.js
@@ -76738,3 +76738,4 @@ a(b.target)}for(var r=0;r<s.length;++r)s[r].addEventListener("load",onLoad,false
 var s=A.afV
 if(typeof dartMainRunner==="function")dartMainRunner(s,[])
 else s([])})})()
+//# sourceMappingURL=main.dart.js.map
